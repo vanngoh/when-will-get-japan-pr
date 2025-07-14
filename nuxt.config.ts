ad57069c500d30c864d4120a1a7309639837f513
Vann <vanngoh.dev@gmail.com>
2025-07-14 19:34:12 +0900

    update the dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,28 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
-  modules: [
-    '@nuxt/ui',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode'
-  ],
-  colorMode: {
-    preference: 'system',
-    fallback: 'light',
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '',
-    storageKey: 'nuxt-color-mode'
-  },
-  ui: {
-    global: true,
-  },
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: 'tailwind.config.ts',
-    exposeConfig: false,
-    viewer: true
-  }
-}) 
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css']
+})
